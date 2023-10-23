c34a96bfb9e88b3f4ff5622e37ed1609a0d76454
MichaelGift <simiyugift@outlook.com>
2023-10-23 22:09:27 +0300

    Request storage permission

--- app/src/main/java/com/next/sync/MainActivity.kt
+++ app/src/main/java/com/next/sync/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.next.sync
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.next.sync.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -31,5 +35,35 @@ class MainActivity : AppCompatActivity() {
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+        promptUserForPermissions()
     }
+
+    private fun promptUserForPermissions() {
+        val permissionsToRequest = arrayOf(
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.INTERNET,
+            Manifest.permission.WAKE_LOCK,
+            Manifest.permission.BATTERY_STATS,
+            Manifest.permission.REQUEST_IGNORE_BATTERY_OPTIMIZATIONS,
+        )
+
+        val permissionsGranted = permissionsToRequest.all { permission ->
+            ContextCompat.checkSelfPermission(this, permission) == PackageManager.PERMISSION_GRANTED
+        }
+
+        if (permissionsGranted) {
+            // All permissions are already granted, proceed with your operation.
+        } else {
+            ActivityCompat.requestPermissions(
+                this, permissionsToRequest, REQUEST_PERMISSIONS
+            )
+        }
+    }
+
+
+    companion object {
+        private const val REQUEST_PERMISSIONS = 101
+    }
+
 }
