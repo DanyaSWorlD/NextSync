761df5996001f490d465ccf0f2237774c5e00e1f
Michael Gift <simiyugift@outlook.com>
2024-05-25 15:34:47 +0300

    Create state and events for home screen

--- app/src/main/java/com/next/sync/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/next/sync/ui/home/HomeViewModel.kt
@@ -1,13 +1,31 @@
 package com.next.sync.ui.home
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import com.next.sync.ui.events.HomeEvents
+
+
+data class HomeState(
+    val isSynchronizing: Boolean = false,
+    val isUsingWifi: Boolean = false,
+    val isBatteryCharging: Boolean = false,
+    val lastSync: String = "",
+    val nextSync: String = "",
+    val allTimeUpload: String = "",
+    val allTimeDownload: String = "",
+    val storageUsed: Int = 0,
+    val storageTotal: Int = 0,
+)
 
 class HomeViewModel : ViewModel() {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
+    var homeState by mutableStateOf(HomeState())
+
+    fun onEvent(event: HomeEvents) {
+        when (event) {
+            HomeEvents.SynchronizeNow -> {}
+        }
     }
-    val text: LiveData<String> = _text
 }
