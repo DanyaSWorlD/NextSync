40713bf3a9f266c3e55b98200be26425fed265ab
Michael Gift <simiyugift@outlook.com>
2024-05-26 01:43:26 +0300

    Create LoginState to handle changing data.
    - Add event to handle typing changes for server address

--- app/src/main/java/com/next/sync/ui/login/LoginViewModel.kt
+++ app/src/main/java/com/next/sync/ui/login/LoginViewModel.kt
@@ -7,21 +7,27 @@ import androidx.lifecycle.ViewModel
 import com.next.sync.ui.events.LoginEvents
 
 
+//Can be renamed
+data class LoginState(
+    val isLoggedIn: Boolean = false,
+    val serverAddress: String = "",
+    var loginFlow: String = "$serverAddress/index.php/login/flow"
+)
 class LoginViewModel : ViewModel() {
-    var serverAddress by mutableStateOf("")
-    val loginFlow : String
-        get() = serverAddress + "/index.php/login/flow"
-
-    fun changeServerAddress(value: String) {
-        serverAddress = value
-    }
-
-
+    var loginState by mutableStateOf(LoginState())
 
     fun onEvent(event: LoginEvents) {
         when (event) {
             is LoginEvents.OnAddressConfirmed -> {
-                // Handle address confirmation
+                // Sample for login
+                // var response = loginService.login(loginState.loginFlow)
+                // if(response == "success"){
+                //     loginState = loginState.copy(isLoggedIn = true)
+                // }
+            }
+
+            is LoginEvents.UpdateServerAddress -> {
+                loginState = loginState.copy(serverAddress = event.address)
             }
         }
     }
