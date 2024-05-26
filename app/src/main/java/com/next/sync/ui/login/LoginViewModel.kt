3e69a5cf0adf8b0196f48cf9e4a36991319c21fa
Michael Gift <simiyugift@outlook.com>
2024-05-26 04:34:06 +0300

    Move appended server address to const val

--- app/src/main/java/com/next/sync/ui/login/LoginViewModel.kt
+++ app/src/main/java/com/next/sync/ui/login/LoginViewModel.kt
@@ -12,18 +12,18 @@ import com.next.sync.ui.events.LoginEvents
 data class LoginState(
     val isLoggedIn: Boolean = false,
     val serverAddress: String = "",
-){
-    val loginFlow: String = "$serverAddress/index.php/login/flow"
-}
+)
+const val loginFlow: String = "/index.php/login/flow"
 class LoginViewModel : ViewModel() {
     var loginState by mutableStateOf(LoginState())
+
 
 
     fun onEvent(event: LoginEvents) {
         when (event) {
             is LoginEvents.OnAddressConfirmed -> {
                 // Sample for login
-                // var response = loginService.login(loginState.loginFlow)
+                // var response = loginService.login(loginState.serverAddress+loginFlow)
                 // if(response == "success"){
                 //     loginState = loginState.copy(isLoggedIn = true)
                 // }
