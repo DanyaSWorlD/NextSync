17ef9515b8e181e81bed854fa237df11ae4d0645
Michael Gift <simiyugift@outlook.com>
2024-06-05 04:58:12 +0300

    Replace service intent for worker creation

--- app/src/main/java/com/next/sync/MainActivity.kt
+++ app/src/main/java/com/next/sync/MainActivity.kt
@@ -7,19 +7,23 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import androidx.work.ExistingPeriodicWorkPolicy
+import androidx.work.PeriodicWorkRequestBuilder
+import androidx.work.WorkManager
+import androidx.work.workDataOf
+import com.next.sync.core.wokers.SyncCheckWorker
 import com.next.sync.ui.AppNavigation
 import com.next.sync.ui.theme.AppTheme
 import dagger.hilt.android.AndroidEntryPoint
+import java.util.concurrent.TimeUnit
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        initWorkers()
         setContent {
-            val intent = Intent(this, MonitoringService::class.java)
-            startService(intent)
-
             AppTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
@@ -29,5 +33,20 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+    }
+
+    private fun initWorkers() {
+        val downloadPath = getDownloadsDirectory().absolutePath
+        val inputData = workDataOf("path" to downloadPath)
+
+        val localSyncWorker = PeriodicWorkRequestBuilder<SyncCheckWorker>(15, TimeUnit.MINUTES)
+            .setInputData(inputData)
+            .build()
+
+        WorkManager.getInstance(this).enqueueUniquePeriodicWork(
+            "DirectoryScanWork",
+            ExistingPeriodicWorkPolicy.UPDATE,
+            localSyncWorker
+        )
     }
 }
