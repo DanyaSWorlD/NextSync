7c3f6f2aa2bc4c59abbd7f6efb1473349fcfb9eb
Michael Gift <simiyugift@outlook.com>
2025-07-22 00:03:07 +0300

    Add NotificationManager Hilt module
    This commit introduces a Dagger Hilt module to provide a `NotificationManagerCompat` instance.
    
    The module:
    - Is a singleton.
    - Creates two notification channels ("Main Channel" and "Second Channel") on Android O and above with default and low importance respectively.
    - Returns a `NotificationManagerCompat` instance.

---
+++ app/src/main/java/com/next/sync/core/di/NotificationManager.kt
@@ -0,0 +1,41 @@
+package com.next.sync.core.di
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
+import androidx.core.app.NotificationManagerCompat
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object Notification {
+    @Singleton
+    @Provides
+    fun provideNotificationManager(
+        @ApplicationContext context: Context
+    ): NotificationManagerCompat {
+        val notificationManager = NotificationManagerCompat.from(context)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                "Main Channel ID",
+                "Main Channel",
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
+            val channel2 = NotificationChannel(
+                "Second Channel ID",
+                "Second Channel",
+                NotificationManager.IMPORTANCE_LOW
+            )
+            notificationManager.createNotificationChannel(channel)
+            notificationManager.createNotificationChannel(channel2)
+        }
+        return notificationManager
+    }
+
+}
